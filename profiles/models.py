ee2b8fe23a1560483c48c2482ab047011a27aef3
Sam Yates-Smith <samyatessmith@gmail.com>
2024-05-17 10:46:22 +0000

    1.1 Added initial model frames for dives, profiles & wishlist.

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from Django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_pic = models.ImageToField(upload_to='profile_pics/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from Django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=255)
+    description = models.TextField()
