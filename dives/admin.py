85bfe2c0a7b9ceca40823b30780d210baf900f00
Sam Yates-Smith <samyatessmith@gmail.com>
2024-05-17 11:15:36 +0000

    1.3 Resolved small bugs. Accessed admin area. Working.

--- dives/admin.py
+++ dives/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Dive
+
+
 # Register your models here.
 
+
+@admin.register(Dive)
 class DiveAdmin(admin.ModelAdmin):
     list_display = ('user', 'location', 'date', 'depth')
     search_fields = ('location', 'buddy')
